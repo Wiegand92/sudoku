603a90f53eca02601dbb2ae0d86b8e77247083a3
Wiegand92 <wiegand.alex@gmail.com>
2022-02-17 00:50:42 -0500

    add completeCell helper function

--- src/utils/completeCell.spec.ts
+++ src/utils/completeCell.spec.ts
@@ -4,7 +4,7 @@ describe("completeCell function", () => {
   it("if there is only one possibility, set the cell to that number", () => {
     const expected = 1;
     completeCell(incompleteSudokuBoard, 8, 7);
-    const actual = incompleteSudokuBoard[8][7]
+    const actual = incompleteSudokuBoard[8][7];
 
     expect(actual).toEqual(expected);
   });
@@ -12,7 +12,7 @@ describe("completeCell function", () => {
   it("if there is more than one possibility, set the cell to an array of possiblities", () => {
     const expected = [3, 4, 6, 7, 8, 9];
     completeCell(incompleteSudokuBoard, 0, 0);
-    const actual = incompleteSudokuBoard[0][0]
+    const actual = incompleteSudokuBoard[0][0];
 
     expect(actual).toEqual(expected);
   });
