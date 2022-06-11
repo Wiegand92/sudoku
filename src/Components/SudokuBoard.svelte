edd92fcc9f99837482a7aef35d2fdca6a48ca18d
Wiegand92 <wiegand.alex@gmail.com>
2022-06-11 15:46:45 -0400

    finish conflicts object, update styles

--- src/Components/SudokuBoard.svelte
+++ src/Components/SudokuBoard.svelte
@@ -1,203 +1,290 @@
-<script lang='ts'>
-    import {fade} from 'svelte/transition'
-    import LoadingScreen from "./LoadingScreen.svelte";
-    import { blockCoordinates } from '../utils/blockCoordinates';
-    import { validMove } from '../utils/checkHouse';
-    // An array of moves for time travel //
-    const moves = [];
-    // playerSolution will contain a copy of the puzzle for the player to make changes to //
-    let playerSolution = [];
-    // These variables will be populated with the puzzle and solution //
-    let puzzle = [];
-    let puzzleSolution = [];
-    // Loading state for puzzle //
-    let puzzleGenerated = false;
+<script lang="ts">
+  import { fade } from "svelte/transition";
+  import LoadingScreen from "./LoadingScreen.svelte";
+  import { blockCoordinates } from "../utils/blockCoordinates";
+  import { validMove } from "../utils/checkHouse";
+  import { getBlock, getColumn, getRow } from "../utils/getSections";
+  import {
+    initialize,
+    puzzle,
+    playerSolution,
+    puzzleSolution,
+    puzzleGenerated,
+    conflicts,
+    updateSolution,
+    updateConflicts,
+  } from "../store";
+  import { cloneBoard } from "../utils/cloneBoard";
+  // An array of moves for time travel //
+  const moves = [];
 
-    // worker will generate puzzle on different thread to avoid blocking //
-    const worker = new Worker(new URL('../utils/worker', import.meta.url));
+  initialize();
 
-    // Generate puzzle //
-    worker.postMessage('')
-
-    // When the worker finishes generation, copy over puzzle and puzzleSolution //
-    worker.onmessage = function (e) {
-        puzzle = e.data.puzzle;
-        puzzleSolution = e.data.puzzleSolution;
-        puzzle.forEach(r=> {
-            const row = [];
-            r.forEach(c => {
-                row.push(c)
-            })
-            playerSolution.push(row)
-        });
-        puzzleGenerated = true;
+  // Returns class list for cells //
+  function getClassName(row: number, col: number) {
+    let className: string = "";
+    const adjustedRow = row + 1;
+    const adjustedCol = col + 1;
+    if (adjustedRow % 3 === 0) {
+      className += "rowDivider ";
     }
-
-    // Returns class list for cells //
-    function getClassName(row: number, col: number) {
-        let className: string = '';
-        const adjustedRow = row + 1;
-        const adjustedCol = col + 1;
-        if(adjustedRow % 3 === 0 ){
-            className += 'rowDivider '
-        }
-        if(adjustedCol % 3 === 0) {
-            className += 'colDivider '
-        }
-        if(adjustedCol === 1) {
-            className += 'firstCol '
-        }
-        if(adjustedRow === 1) {
-            className += 'topRow '
-        }
-        if(adjustedCol === 1 && adjustedRow === 1){
-            className+= 'topL '
-        }
-        if(adjustedCol === 9 && adjustedRow === 1) {
-            className += 'topR '
-        }
-        if(adjustedCol === 1 && adjustedRow === 9) {
-            className += 'botL '
-        }
-        if(adjustedCol === 9 && adjustedRow === 9){
-            className += 'botR '
-        }
-        return className.trim();
+    if (adjustedCol % 3 === 0) {
+      className += "colDivider ";
     }
-
-    // Manipulates playerSolution and moves //
-    function addNumber(row: number, col: number, move: (number | '')){
-        playerSolution[row][col] = move;
-        moves.push({row, col, move});
+    if (adjustedCol === 1) {
+      className += "firstCol ";
     }
+    if (adjustedRow === 1) {
+      className += "topRow ";
+    }
+    if (adjustedCol === 1 && adjustedRow === 1) {
+      className += "topL ";
+    }
+    if (adjustedCol === 9 && adjustedRow === 1) {
+      className += "topR ";
+    }
+    if (adjustedCol === 1 && adjustedRow === 9) {
+      className += "botL ";
+    }
+    if (adjustedCol === 9 && adjustedRow === 9) {
+      className += "botR ";
+    }
+    if ($conflicts !== null) {
+      const { rowConflict, columnConflict, blockConflict, origin } = $conflicts;
+      if (
+        (row === rowConflict.row && col === rowConflict.col) ||
+        (col === columnConflict.col && row === columnConflict.row) ||
+        (col === origin.col && row === origin.row) ||
+        (col === blockConflict.blockCol && row === blockConflict.blockRow)
+      ) {
+        className += "conflict ";
+      }
+    }
+    return className.trim();
+  }
 
-    // Change Handler //
-    function handleChange(e: Event, row: number, col: number){
-        const target = <HTMLInputElement>e.target;
-        const number = Number(target.value);
-        // When number is valid add to board/moves //
-        if(number <= 9 && number > 0) {
-            addNumber(row, col, number);
-            if(!validMove(playerSolution, row, col, blockCoordinates[row][col], number)){
-                console.log('Not a valid move')
+  // Manipulates playerSolution and moves //
+  function addNumber(row: number, col: number, move: number | "") {
+    const newPuzzle = cloneBoard($playerSolution);
+    newPuzzle[row][col] = move || 0;
+    updateSolution(newPuzzle);
+    moves.push({ row, col, move });
+  }
+
+  // Change Handler //
+  function handleChange(e: Event, row: number, col: number) {
+    const target = <HTMLInputElement>e.target;
+    const number = Number(target.value);
+    updateConflicts(null);
+    // When number is valid add to board/moves //
+    if (number <= 9 && number > 0) {
+      if (
+        !validMove(
+          $playerSolution,
+          row,
+          col,
+          blockCoordinates[row][col],
+          number
+        )
+      ) {
+        const columnConflict = getColumn($playerSolution, col).indexOf(number);
+        const rowConflict = getRow($playerSolution, row).indexOf(number);
+        const blockConflict = getBlock(
+          $playerSolution,
+          blockCoordinates[row][col]
+        ).indexOf(number);
+        function getBlockCoords(
+          blockConflict: number,
+          row: number,
+          col: number
+        ) {
+          let blockCol = blockConflict % 3;
+          let blockRow;
+          if (blockConflict > 5) blockRow = 2;
+          else if (blockConflict > 2) blockRow = 1;
+          else blockRow = 0;
+          const blockNum = blockCoordinates[row][col];
+          switch (blockNum) {
+            case 1:
+              blockCol += 3;
+              break;
+            case 2:
+              blockCol += 6;
+              break;
+            case 3:
+              blockRow += 3;
+              break;
+            case 4: {
+              blockCol += 3;
+              blockRow += 3;
+              break;
             }
-        }
-        // When the player clears the box set it to null //
-        else if (target.value === '') addNumber(row, col, '') 
-        // Otherwise set the value to last correct value //
-        else target.value = playerSolution[row][col] || '';
-        if(checkPuzzle()){
-            alert('Congrats you win!!!')
-        }
-    }
-
-    //Check the solution//
-    function checkPuzzle(){
-        let solved = true
-        for(let row = 0; row < 9; row++){
-            // Early break if solved becomes false //
-            if(solved === false){ break; }
-            // Check each value against solution given //
-            for(let col = 0; col < 9; col++){
-                if(puzzleSolution[row][col] !== playerSolution[row][col]){
-                    solved = false
-                    break;
-                }
+            case 5: {
+              blockCol += 6;
+              blockRow += 3;
+              break;
             }
+            case 6: {
+              blockRow += 6;
+              break;
+            }
+            case 7: {
+              blockCol += 3;
+              blockRow += 6;
+              break;
+            }
+            case 8: {
+              blockCol += 6;
+              blockRow += 6;
+              break;
+            }
+          }
+          return { blockRow, blockCol };
         }
-        return solved;
-    }
 
-    function newGame() {
-        playerSolution = []
-        puzzleGenerated = false;
-        worker.postMessage('')
-    }
+        const newConflicts = {
+          origin: { row, col },
+          rowConflict: { col: rowConflict, row },
+          columnConflict: { row: columnConflict, col },
+          blockConflict: getBlockCoords(blockConflict, row, col),
+        };
 
+        updateConflicts(newConflicts);
+        console.log($conflicts);
+      }
+      addNumber(row, col, number);
+    }
+    // When the player clears the box set it to null //
+    else if (target.value === "") addNumber(row, col, "");
+    // Otherwise set the value to last correct value //
+    else {
+      console.log("Numbers only!");
+      target.value = $playerSolution[row][col];
+    }
+    if (checkPuzzle()) {
+      alert("Congrats you win!!!");
+    }
+  }
+
+  //Check the solution//
+  function checkPuzzle() {
+    let solved = true;
+    for (let row = 0; row < 9; row++) {
+      // Early break if solved becomes false //
+      if (solved === false) {
+        break;
+      }
+      // Check each value against solution given //
+      for (let col = 0; col < 9; col++) {
+        if ($puzzleSolution[row][col] !== $playerSolution[row][col]) {
+          solved = false;
+          break;
+        }
+      }
+    }
+    return solved;
+  }
 </script>
+
 <section>
-    {#if puzzleGenerated}
-    <div class='puzzleGrid' transition:fade>
-    {#each puzzle as row, rowIndex}
-        {#each row as column, colIndex}
+  {#if $puzzleGenerated}
+    <div class="puzzleGrid" transition:fade>
+      {#each $playerSolution as row, rowIndex}
+        {#each row as column, colIndex ([rowIndex, colIndex])}
+          {#if $puzzle[rowIndex][colIndex] !== 0}
             <span class={getClassName(rowIndex, colIndex)}>
-                {#if column !== 0}
-                <p>{column}</p>
-                {:else}
-                <input 
-                    type='number' 
-                    min=1 
-                    max=9 
-                    value={playerSolution[rowIndex][colIndex] || ''} 
-                    on:change={e=>{
-                        handleChange(e, rowIndex, colIndex)
-                    }} 
-                />
-                {/if}
+              <p>{column}</p>
             </span>
+          {:else}
+            <input
+              type="number"
+              inputmode="numeric"
+              min="1"
+              max="9"
+              value={$playerSolution[rowIndex][colIndex] || ""}
+              on:change={(e) => {
+                handleChange(e, rowIndex, colIndex);
+              }}
+              class={getClassName(rowIndex, colIndex)}
+            />
+          {/if}
         {/each}
-    {/each}
+      {/each}
     </div>
-    {:else}
-    <LoadingScreen></LoadingScreen>
-    {/if}
-    <button on:click={newGame} disabled={!puzzleGenerated}>New Game</button>
+  {:else}
+    <LoadingScreen />
+  {/if}
+  <button on:click={initialize} disabled={!$puzzleGenerated}>New Game</button>
 </section>
-<style lang='postcss'>
-    section{
-        @apply h-full flex flex-col place-content-around;
-    }
-    .puzzleGrid{
-        @apply inline-grid flex-initial grid-cols-9 m-auto shadow-md;
-    }
-    span{
-        @apply box-border h-8 aspect-square;
-        @apply border-solid border-black border-[1px] flex place-content-center;
-        @apply md:h-20;
-    }
-    p{
-        @apply m-auto;
-    }
-    input{
-        @apply m-0 p-0 border-0 w-full text-center;
-    }
-    p, input {
-        @apply text-3xl font-mono;
-    }
-    /* Remove number input spinners */
-    /* Chrome, Safari, Edge, Opera */
-    input::-webkit-outer-spin-button,
-    input::-webkit-inner-spin-button {
+
+<style lang="postcss">
+  section {
+    @apply h-full flex flex-col place-content-around;
+  }
+  .puzzleGrid {
+    @apply inline-grid flex-initial grid-cols-9 m-auto shadow-lg rounded-md bg-black;
+  }
+  span,
+  input {
+    @apply h-10 w-10 aspect-square p-0;
+    @apply border-solid border-black border-[1px] flex place-content-center bg-white;
+    @apply md:h-20 md:w-20;
+  }
+  .conflict p,
+  .conflict {
+    @apply text-red-600;
+  }
+  p {
+    @apply m-auto p-0;
+  }
+  input[type="number"] {
+    @apply m-0 p-0 text-center rounded-none;
+  }
+  input[type="number"]:focus {
+    @apply bg-slate-500 outline-none;
+  }
+  p,
+  input[type="number"] {
+    @apply text-3xl font-mono;
+  }
+  /* Remove number input spinners */
+  /* Chrome, Safari, Edge, Opera */
+  input::-webkit-outer-spin-button,
+  input::-webkit-inner-spin-button {
     -webkit-appearance: none;
     margin: 0;
-    }
-    /* Firefox */
-    input[type=number] {
+  }
+  /* Firefox */
+  input[type="number"] {
     -moz-appearance: textfield;
-    }
-    .rowDivider{
-        @apply border-b-4;
-    }
-    .colDivider{
-        @apply border-r-4;
-    }
-    .topRow {
-        @apply border-t-4;
-    }
+  }
+  .rowDivider {
+    @apply border-b-4;
+  }
+  .colDivider {
+    @apply border-r-4;
+  }
+  .topRow {
+    @apply border-t-4;
+  }
 
-    .firstCol {
-        @apply border-l-4;
-    }
-    .topR, .topR:first-child {
-        @apply rounded-tr-md;
-    }
-    .topL, .topL:first-child {
-        @apply rounded-tl-md;
-    }
-    .botR, .botR:first-child{
-        @apply rounded-br-md;
-    }
-    .botL, .botL:first-child {
-        @apply rounded-bl-md;
-    }
+  .firstCol {
+    @apply border-l-4;
+  }
+  .topR,
+  .topR p {
+    @apply !rounded-tr-md;
+  }
+  .topL,
+  .topL p {
+    @apply !rounded-tl-md;
+  }
+  .botR,
+  .botR p {
+    @apply !rounded-br-md;
+  }
+  .botL,
+  .botL p {
+    @apply !rounded-bl-md;
+  }
 </style>
--- src/store.ts
+++ src/store.ts
@@ -1,8 +1,49 @@
-import { readable, writable } from "svelte/store";
+import { readable, writable, get } from "svelte/store";
+import { cloneBoard } from "./utils/cloneBoard";
 
 const puzzleSolution = writable([]);
 const puzzle = writable([]);
-const playerSolution = writable([]);
+const playerSolution = writable(null);
+const puzzleGenerated = writable(false);
+const conflicts = writable(null);
 const moves = writable([]);
 const solved = writable(false);
 const timeStarted = 0;
+
+function initialize() {
+  if (get(puzzleGenerated) === true) puzzleGenerated.set(false);
+  if (get(conflicts) !== null) conflicts.set(null);
+  // worker will generate puzzle on different thread to avoid blocking //
+  const worker = new Worker(new URL("./utils/worker", import.meta.url));
+
+  // Generate puzzle //
+  worker.postMessage("");
+
+  // When the worker finishes generation, copy over puzzle and puzzleSolution //
+  worker.onmessage = function (e) {
+    puzzle.set(e.data.puzzle);
+    puzzleSolution.set(e.data.puzzleSolution);
+    playerSolution.set(cloneBoard(e.data.puzzle));
+    puzzleGenerated.set(true);
+    worker.terminate();
+  };
+}
+
+function updateSolution(newBoard) {
+  playerSolution.set(newBoard);
+}
+
+function updateConflicts(newConflicts) {
+  conflicts.set(newConflicts);
+}
+
+export {
+  initialize,
+  puzzleSolution,
+  puzzle,
+  playerSolution,
+  puzzleGenerated,
+  conflicts,
+  updateSolution,
+  updateConflicts,
+};
